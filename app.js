9b562dc84ae9933d8dadc4594cd4ccc12c3d4d29
CodeByBlazej <codebyblazej@gmail.com>
2023-12-14 18:51:35 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 const ratingEls = document.querySelectorAll('.rating');
 const backdrop = document.getElementById('backdrop');
+const btnEl = document.getElementById('btn');
+const containerEl = document.getElementById('container')
 
+let selectedRating = '';
 
 
 ratingEls.forEach((ratingEl) => {
   ratingEl.addEventListener('click', (event) => {
     event.stopPropagation();
     removeActive();
+    selectedRating =
+      event.target.innerText || event.target.parentNode.innerText;
     event.target.classList.add('active');
     event.target.parentNode.classList.add('active');
   });
 });
+
+btnEl.addEventListener('click', () => {
+  if (selectedRating !== '') {
+    containerEl.innerHTML = `
+    <strong>Thank you!</strong>
+    <br>
+    <br>
+    <strong>Feedback: ${selectedRating}</strong>
+    <p>We'll use your feedback to improve our customer support.</p>
+    `
+  }
+})
 
 
 function removeActive() {
